7bdaad2843a2869df4a571baed6d5dcd18676fa5
YQh9Mh3181jzEJ5 <mfghz487k44o6u6ekgub@gmail.com>
2024-02-04 20:12:02 +0900

    feat: Add About page

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "study-react",
+  title: "Next.jsで作るブログです！",
   description: "stydying react",
 };
 
